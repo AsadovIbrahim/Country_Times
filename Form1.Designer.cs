8b41549130a920a6568050ebd30e8ac285b41c93
Ibrahim <ibrahimasadov32@gmail.com>
2023-05-28 16:12:02 +0400

    Countries_Time

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using Images.Properties;
+namespace Images
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            Baku = new Button();
+            London = new Button();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // Baku
+            // 
+            Baku.Location = new Point(0, 0);
+            Baku.Name = "Baku";
+            Baku.Size = new Size(180, 74);
+            Baku.TabIndex = 0;
+            Baku.Text = "Baku";
+            Baku.UseVisualStyleBackColor = true;
+            Baku.Click += Baku_Click;
+            // 
+            // London
+            // 
+            London.Location = new Point(620, 0);
+            London.Name = "London";
+            London.Size = new Size(177, 74);
+            London.TabIndex = 1;
+            London.Text = "London";
+            London.UseVisualStyleBackColor = true;
+            London.Click += London_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.baku;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 450);
+            Controls.Add(London);
+            Controls.Add(Baku);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private BindingSource bindingSource1;
+        private Button Baku;
+        private Button London;
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Images.Properties;
+using System.Timers;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Images
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1000; 
+            timer1.Tick += Timer_Tick;
+           
+            timer2 = new System.Windows.Forms.Timer();
+            timer2.Interval = 1000; 
+            timer2.Tick += Timer_Tick;
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Text = DateTime.Now.ToLongTimeString();
+        }
+        private void Timer_Tick1(object sender, EventArgs e)
+        {
+            Text = DateTime.Now.AddHours(-3).ToLongTimeString();
+        }
+        private void Baku_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            timer1.Interval = 1000;
+            timer1.Tick += Timer_Tick;
+            Text = DateTime.Now.ToLongTimeString();
+            BackgroundImage = Resources.baku;
+            timer1.Start();
+        }
+        private void London_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Interval = 1000;
+            timer2.Tick += Timer_Tick1;
+            Text = DateTime.Now.AddHours(-3).ToLongTimeString();
+            BackgroundImage = Resources.london;
+            timer2.Start();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
